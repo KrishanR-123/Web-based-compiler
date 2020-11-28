e823953602ab76ed6608943ff5e91c2e76c4455e
Payal Singla <singlapayal43@gmail.com>
2020-11-29 00:23:02 +0530

    Create data.h

---
+++ Version 6 Comparisions/data.h
@@ -0,0 +1,13 @@
+#ifndef extern_
+#define extern_ extern
+#endif
+
+
+
+extern_ int Line;			// Current line number
+extern_ int Putback;			// Character put back by scanner
+extern_ FILE *Infile;			// Input and output files
+extern_ FILE *Outfile;
+extern_ struct token Token;		// Last token scanned
+extern_ char Text[TEXTLEN + 1];		// Last identifier scanned
+extern_ struct symtable Gsym[NSYMBOLS];	// Global symbol table
